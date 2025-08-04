835b7db6c2c8aad3a09c289a57e96f01e3bbdb27
kerly suarez <kerly_suarezb@hotmail.com>
2025-08-03 22:40:07 -0500

    Modifica app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Declaramos el array para almacenar los nombres de amigos
+let amigos = [];
